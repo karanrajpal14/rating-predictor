a96dabf2f57d8e8e89e00332fa4122ace40aabc0
Karan Rajpal <rajpal.karan73@gmail.com>
2020-04-25 15:33:17 -0500

    Completed basic AWS EB config

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"hello": "world", "from": "index"})
-
-@app.route("/foo")
-def foo():
-    return jsonify({"hello": "world", "from": "foo"})
+    return jsonify({"project": "Review Rating Predictor", "class": "CSE 5334 002", "course": "Data Mining"})
 
 if __name__ == '__main__':
     app.run(debug=True)
